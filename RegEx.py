ee9d60c8c82b5264d270d416d91a7f7df3d4a365
GourabRoy551 <gourabroysec553@gmail.com>
2020-12-08 11:06:13 +0600

    PyCode

--- RegEx.py
+++ RegEx.py
@@ -130,3 +130,54 @@ print(randStr)
 # \t : Tab
 # \v : Vertical Tab
 # To remove \r \n on Windows
+
+# --------- Match any Single Number ----------
+# \d can be used instead of [0-9]
+# \d is the same as [^0-9]
+
+randStr = "12345"
+
+print("Matches : ", len(re.findall("\d", randStr)))
+
+# ------------ Match Multiple Numbers -------------------
+# Can match multiple digits by following the \d with {numofValues}
+
+# Match 5 numbers only
+if re.search("\d{5}", "12345"):
+    print("It is a zip code")
+
+# We can also match within a range
+# Match values that are between 5 and 7 digits
+
+numStr = "123 12345 123456 1234567"
+
+print("Matches : ", len(re.findall("\d{5,7}", numStr)))
+
+# ---------Matching Any Single Letter or Number ------------
+# \w is the same as [a-zA-Z0-9_]
+# \W is the same as [^a-zA-Z0-9_]
+
+phNum = "412-555-1212"
+
+# Check if it is a phone number
+
+if re.search("\w{3}-w{3}-w{4}", phNum):
+    print("It is a phone number")
+
+# Check for valid first name between 2 and 20 characters
+if re.search("\w{2,20}", "Ultraman"):
+    print("It is a valid name")
+
+# -----------Matching Whitespace ----------------
+# \s is the same as [\f\n\r\t\v]
+# \S is the same as [^\f\n\r\t\v]
+
+# Check for valid first and last name with a space
+if re.search("\w{2,20}\s\w{2,20}", "Toshio Muramatsu"):
+    print("It is a valid name")
+
+# -----------Matching One or More ----------------
+# + matches 1 pr more characters
+
+print("Matches : ", len(re.findall("a+", "a as ape bug")))
+print("Matches : ", re.findall("a+", "a as ape bug"))
