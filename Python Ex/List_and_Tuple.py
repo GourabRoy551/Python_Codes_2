ca396ead7ba69d3df633449ca539882b8bd0108c
GourabRoy551 <gourabroysec553@gmail.com>
2020-12-10 12:25:14 +0600

    PyCode

---
+++ Python Ex/List_and_Tuple.py
@@ -0,0 +1,6 @@
+values = input("Input some comma seprated numbers : ")
+list = values.split(",")
+tuple = tuple(list)
+
+print("List : ", list)
+print("Tuple : ", tuple)
---
+++ Python Ex/Reverse_Order.py
@@ -0,0 +1,14 @@
+"""
+Python has two functions designed for accepting data directly from the user:
+        1.Python 2.x. -> raw_input()
+        2.Python 3.x. -> input()
+"""
+
+fname = input("Input your First Name : ")
+lname = input("Input your Last Name : ")
+print ("Hello  {0} {1}".format(lname, fname))
+
+str="Welcome"
+
+x=str[::-1]
+print(x)
