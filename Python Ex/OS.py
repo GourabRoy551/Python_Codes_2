5b64b5d939f3e8bfb240443731f50878899dd7c1
GourabRoy551 <gourabroysec553@gmail.com>
2020-12-12 14:01:26 +0600

    PyCode

---
+++ Python Ex/OS.py
@@ -0,0 +1,13 @@
+import platform
+import os
+
+print(os.name)
+print(platform.system())
+print(platform.release())
+
+"""
+Output:
+       nt
+       Windows
+       10 
+"""
---
+++ Python Ex/PRofile.py
@@ -0,0 +1,23 @@
+import cProfile
+
+
+def sum():
+    print(1 + 2)
+
+
+cProfile.run('sum()')
+
+"""
+Output:
+3
+ 5 function calls in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+        1    0.000    0.000    0.000    0.000 PRofile.py:4(sum)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' 
+"""
