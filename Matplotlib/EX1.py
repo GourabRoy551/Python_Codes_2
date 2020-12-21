def193b1a7714cf0e9146dfcf3a1c0722a462dc8
GourabRoy551 <gourabroysec553@gmail.com>
2020-12-21 17:07:53 +0600

    PyCode

---
+++ Matplotlib/EX1.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+x = np.linspace(0, 10, 20)
+y = x*x
+z = x+y
+
+plt.plot(x, y)
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+plt.title('No need')
+plt.show()
