9bf1d4f7e69c6e4a1da701d580545dbe3dacf2f5
GourabRoy551 <gourabroysec553@gmail.com>
2021-01-02 23:25:23 +0600

    PyCode

--- Python Ex/OS.py
+++ Python Ex/OS.py
@@ -1,13 +1,11 @@
-import platform
-import os
+upper = 10
 
-print(os.name)
-print(platform.system())
-print(platform.release())
+print("Prime numbers between", "and", upper, "are:")
 
-"""
-Output:
-       nt
-       Windows
-       10 
-"""
+for num in range(upper + 1):
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
