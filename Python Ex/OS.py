8312a692487c11772ba5dcca9ac4866ef42b9050
GourabRoy551 <gourabroysec553@gmail.com>
2021-01-05 21:19:49 +0600

    PyCode

--- Python Ex/OS.py
+++ Python Ex/OS.py
@@ -1,11 +1,26 @@
-upper = 10
+from array import *
 
-print("Prime numbers between", "and", upper, "are:")
 
-for num in range(upper + 1):
-   if num > 1:
-       for i in range(2, num):
-           if (num % i) == 0:
-               break
-       else:
-           print(num)
+def findNumber(x, y):
+    k = 0
+    for j in x:
+        if j == y:
+            print('Yes')
+        else:
+            print('No')
+            break
+
+        k++1
+
+arr = array('i', [])
+n = int(input())
+
+for i in range(n):
+    arr.append(int(input()))
+
+val = int(input())
+result = findNumber(arr, val)
+
+
+
+
